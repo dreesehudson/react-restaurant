779b76c33efecf1c609a6e2fc89fc8ab2125d88f
Reese Hudson <david.reese.hudson@gmail.com>
2020-10-21 15:22:45 -0400

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './Components/Header.js'
+import Special from './Components/Special.js'
+import Apps from './Components/Apps.js'
+import Lunch from './Components/Lunch.js'
+import Entrees from './Components/Entrees.js';
+import Sides from './Components/Sides.js'
+import Desserts from './Components/Desserts.js'
+import Staff from './Components/Staff'
+import { /*Components*/ } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
 
-    </div>
-  );
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <Staff />
+        <Special />
+        <Apps />
+        <Lunch />
+        <Entrees />
+        <Sides />
+        <Desserts />
+      </div>
+    );
+  }
 }
 
 export default App;
